dd8a42ef60beff0fe6a16611f916230167a4c6c6
IanEllimanJCU <ian.elliman@my.jcu.edu.au>
2020-08-29 00:15:23 +1000

    change broken_score.py to use functions

--- prac_03/broken_score.py
+++ prac_03/broken_score.py
@@ -2,13 +2,21 @@
 CP1404/CP5632 - Practical
 Task 2
 """
-score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    print('Score is invalid')
-elif score >= 90:
-    print('Score is excellent')
-elif score >= 50:
-    print('Score is pass')
-else:
-    print('Score is bad')
-exit()
+
+
+def main():
+    score = float(input("Enter score: "))
+    graded_score = grade_the_score(score)
+    print(graded_score)
+
+def grade_the_score(score):
+    if score < 0 or score > 100:
+        graded_score = 'Score is invalid'
+    elif score >= 90:
+        graded_score = 'Score is excellent'
+    elif score >= 50:
+        graded_score = 'Score is pass'
+    else:
+        graded_score = 'Score is bad'
+    return graded_score
+main()
