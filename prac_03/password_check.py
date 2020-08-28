872fb92d4579e002f8f529447a2ed59822b5cd5e
IanEllimanJCU <ian.elliman@my.jcu.edu.au>
2020-08-28 22:43:06 +1000

    Change to use password_check.py to use functions

--- prac_03/password_check.py
+++ prac_03/password_check.py
@@ -1,7 +1,18 @@
 def main():
-        min_length = 6
-        password = input("Please input a password that's at least 6 characters long: ")
-        while len(password) < 6:
-            password = input("Password too short. Please input a password that's at least 6 characters long: ")
-        print("*" * len(password))
+    min_length = 6
+    password = get_password()
+    while len(password) < 6:
+        password = input("Password too short. Please input a password that's at least 6 characters long: ")
+    passwords_to_asteriks(password)
+
+
+def get_password():
+    password = input("Please input a password at least 6 characters long: ")
+    return password
+
+
+def passwords_to_asteriks(password):
+    return print("*" * len(password))
+
+
 main()
