991931e6b7789ae5e12d3611bc49fcd00e5789bd
IanEllimanJCU <ian.elliman@my.jcu.edu.au>
2020-09-03 22:34:35 +1000

    add security checker

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -8,5 +8,12 @@ def main():
     print(f"The smallest number is {numbers[0]}")
     print(f"The smallest number is {numbers[4]}")
     print(f"The average of the numbers is {sum(numbers)/len(numbers)}")
-
+    usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface',
+                 'BaseStdIn', 'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer',
+                 'bob']
+    username = input("Please input your username: ")
+    if username in usernames:
+        print("Access granted")
+    else:
+        print("Access denied")
 main()
