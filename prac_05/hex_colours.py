c24368b90feca1815062aed8606e7755e0de8510
IanEllimanJCU <ian.elliman@my.jcu.edu.au>
2020-09-03 14:47:44 +1000

    add hex_colours.py in progress

---
+++ prac_05/hex_colours.py
@@ -0,0 +1,3 @@
+COLOUR_CODE = {"maroon": "#b03060", "orchid": "#da70d6", "pink": "#ffc0cb", "salmon": "#fa8072",
+                "thistle": "#d8bfd8", "violet": "#ee82ee", "wheat": "#f5deb3", "linen": "#faf0e6",
+                "lavender": "#e6e6fa", "brown": "#a52a2a"}
