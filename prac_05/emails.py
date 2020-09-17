00b85e67036b8477b2732c80025965e5f324080a
IanEllimanJCU <ian.elliman@my.jcu.edu.au>
2020-09-17 21:06:32 +1000

    refactor emails.py to be simpler

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -6,33 +6,16 @@ def main():
     user_email = input("Email: ")
     while user_email != "":
         user_name_list = user_email.split("@")
-        if "." in user_name_list[0]:
-            user_name_list = user_name_list[0].split(".")
-            print(f"Is your name ", end='')
-            for i in user_name_list:
-                print(f"{i.title()}", end='')
-                if i != user_name_list:
-                    print(" ", end='')
-            print("? (Y/n) ", end='')
-            name_correct = input().upper()
-            if name_correct == "Y":
-                user_name = user_name_list[0] + " " + user_name_list[1]
-            else:
-                user_name = input("Name: ")
-            user_emails_dict.update({user_email:user_name})
-            print(user_emails_dict)
-            user_email = input("Email: ")
+        user_name_list = user_name_list[0].split(".")
+        name = " ".join(user_name_list).title()
+        print(f"Is your name {name}? (Y/n): ", end='')
+        name_correct = input().upper()
+        if name_correct == "Y":
+            user_name = name
         else:
-            print(f"Is your name {user_name_list[0].title()}", end='')
-            print("? (Y/n) ", end='')
-            name_correct = input().upper()
-            if name_correct == "Y":
-                user_name = user_name_list[0]
-            else:
-                user_name = input("Name: ")
-            user_emails_dict.update({user_email:user_name})
-            print(user_emails_dict)
-            user_email = input("Email: ")
+            user_name = input("Name: ")
+        user_emails_dict.update({user_email:user_name})
+        user_email = input("Email: ")
     for i in user_emails_dict:
         print(f"{user_emails_dict[i].title()} ({i})")
 main()
