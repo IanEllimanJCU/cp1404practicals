de457e266c868aae2a88980db962a4e1ad216e45
IanEllimanJCU <ian.elliman@my.jcu.edu.au>
2020-09-17 18:43:01 +1000

    change guitars.py printing of guitars list and add some formatting to output

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -13,9 +13,11 @@ def main():
     guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
     guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
     print("These are my guitars:")
-    i=0
-    for elem in guitars:
-        i = i + 1
-        print(f"Guitar {i}: {elem}")
+    for i, guitar in enumerate(guitars, 0):
+        if guitar.is_vintage == True:
+            vintage_status = "(vintage)"
+        else:
+            vintage_status = ""
+        print(f"Guitar {i + 1}: {guitar.name:>20} ({guitar.year}) : worth ${guitar.cost:10,.2f} {vintage_status}")
 
 main()
