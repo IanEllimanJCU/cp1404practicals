03c1dd93734092bda8928d5477d8bacda1f9a77f
IanEllimanJCU <ian.elliman@my.jcu.edu.au>
2020-09-17 21:42:28 +1000

    correct version

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -5,52 +5,17 @@ def main():
     user_name_list = []
     user_email = input("Email: ")
     while user_email != "":
-        user_name = user_email.split("@")
-        print(user_name)
-        user_name = user_name[0].split(".")
-        print(f"Is your name ", end='')
-        length_user_name = len(user_name)
-        for i in :
-            print(f"{user_name[i].title()}")
-        print("? (Y/n) ", end='')
+        user_name_list = user_email.split("@")
+        user_name_list = user_name_list[0].split(".")
+        name = " ".join(user_name_list).title()
+        print(f"Is your name {name}? (Y/n): ", end='')
         name_correct = input().upper()
         if name_correct == "Y":
-            user_name = user_name[0] + " " + user_name[1]
+            user_name = name
         else:
             user_name = input("Name: ")
-        user_emails_dict.update({user_email: user_name})
-        print(user_emails_dict)
+        user_emails_dict.update({user_email:user_name})
         user_email = input("Email: ")
     for i in user_emails_dict:
         print(f"{user_emails_dict[i].title()} ({i})")
-
 main()
-
-
-"""        if "." in user_name_list[0]:
-            user_name_list = user_name_list[0].split(".")
-            print(f"Is your name ", end='')
-            for i in user_name_list:
-                print(f"{i.title()}", end='')
-                if i != user_name_list:
-                    print(" ", end='')
-            print("? (Y/n) ", end='')
-            name_correct = input().upper()
-            if name_correct == "Y":
-                user_name = user_name_list[0] + " " + user_name_list[1]
-            else:
-                user_name = input("Name: ")
-            user_emails_dict.update({user_email:user_name})
-            print(user_emails_dict)
-            user_email = input("Email: ")
-        else:
-            print(f"Is your name {user_name_list[0].title()}", end='')
-            print("? (Y/n) ", end='')
-            name_correct = input().upper()
-            if name_correct == "Y":
-                user_name = user_name_list[0]
-            else:
-                user_name = input("Name: ")
-            user_emails_dict.update({user_email:user_name})
-            print(user_emails_dict)
-            user_email = input("Email: ")"""
